9d0c2af7027313734b826eeb3720f5c3f64c3317
Jan Podgorski <j.pod.12@gmail.com>
2018-06-28 21:26:07 +0200

    minor language bug fixed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -113,7 +113,7 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-        it('ensures that contect is changing', function () {
+        it('ensures that content is changing', function () {
             contentSecond = feed.html();
             expect(contentFirst).not.toBe(contentSecond);
         });
